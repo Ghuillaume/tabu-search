e2dddfd5773ca71ffadd042fa502aa34602be817
Guillaume CHARON <ghuillaume@live.fr>
2013-02-18 17:36:09 +0100

    Init

---
+++ src/Projet.java
@@ -0,0 +1,22 @@
+import ExamplesJaCoP.BabySitting;
+import JaCoP.core.*;
+import JaCoP.constraints.*;
+import JaCoP.search.*;
+
+
+public class Projet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		BabySitting example = new BabySitting();
+		example.model();
+		Store store = example.getStore();
+		
+		
+
+	}
+
+}
