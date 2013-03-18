f42ee88315fe000d9d61f5ba3265cdd7dd140788
Guillaume CHARON <ghuillaume@live.fr>
2013-03-18 18:43:57 +0100

    Compare performances and add restart procedure

--- src/Projet.java
+++ src/Projet.java
@@ -8,65 +8,118 @@ import JaCoP.search.*;
 
 public class Projet {
 
-	/**
-	 * @param args
-	 */
+	public static void initializeProblem(Store store, ArrayList<Var> vars, IntVar[] queens, int numberQ) {
+		// I-th queen variable represents the placement
+				// of a queen in i-th column
+				// There are n columns so there are n variables
+
+				// Each queen variable has a domain from 1 to numberQ
+				// Value of queen variable represents the row
+				for (int i = 0; i < numberQ; i++) {
+					queens[i] = new IntVar(store, "Q" + (i + 1), 1, numberQ);
+					vars.add(queens[i]);
+				}
+				// Queens from different columns can not be placed
+				// in the same row, therefore the values
+				// must be different
+				for (int i = 0; i < queens.length; i++)
+					for (int j = i - 1; j >= 0; j--)
+						store.impose(new XneqY(queens[i], queens[j]));
+
+				// Notice that j index starts from i+1
+				for (int i = 0; i < queens.length; i++) {
+					for (int j = i + 1; j < queens.length; j++) {
+
+						// Temporary variable denotes the chessboard
+						// field in j-th column which is checked by
+						// i-th column queen
+						// If temporarty variable has value outside
+						// range 1..numberQ then i-th column queen
+						// does not check any field in j-th column
+
+						// Checking diagonals like this \
+						// Note that C constant is positive
+						IntVar temporary = new IntVar(store, -2 * numberQ, 2 * numberQ);
+						store.impose(new XplusCeqZ(queens[j], j - i, temporary));
+						store.impose(new XneqY(queens[i], temporary));
+
+						// Checking diagonals like this /
+						// Note that C constant is negative
+						temporary = new IntVar(store, -2 * numberQ, 2 * numberQ);
+						store.impose(new XplusCeqZ(queens[j], -(j - i), temporary));
+						store.impose(new XneqY(queens[i], temporary));
+
+					}
+				}
+	}
+	
+	public static boolean completeSearch(Store store, IntVar[] Q) {
+		DepthFirstSearch<IntVar> search = new DepthFirstSearch<IntVar>();
+
+		search.getSolutionListener().searchAll(true);
+		search.getSolutionListener().recordSolutions(true);
+
+		SelectChoicePoint<IntVar> select =
+				new SimpleSelect<IntVar>(Q, new SmallestDomain<IntVar>(), new IndomainMedian<IntVar>());
+
+		boolean result = search.labeling(store, select);
+
+		for (int i=1; i<=search.getSolutionListener().solutionsNo(); i++){
+			System.out.print("Solution " + i + ": [");
+			for (int j=0; j<search.getSolution(i).length; j++) {
+				if (j!=0) System.out.print(", ");
+			    System.out.print(search.getSolution(i)[j]);
+			}
+			System.out.println("]");
+		}
+		
+		return result;
+	}
+	
+	
 	public static void main(String[] args) {
 		
-		int numberQ = 7;
+		int numberQ = 10;
 
 		// Creating constraint store
 		Store store = new Store();
 		ArrayList<Var> vars = new ArrayList<Var>();
-
-		// I-th queen variable represents the placement
-		// of a queen in i-th column
-		// There are n columns so there are n variables
 		IntVar queens[] = new IntVar[numberQ];
 
-		// Each queen variable has a domain from 1 to numberQ
-		// Value of queen variable represents the row
-		for (int i = 0; i < numberQ; i++) {
-			queens[i] = new IntVar(store, "Q" + (i + 1), 1, numberQ);
-			vars.add(queens[i]);
-		}
-		// Queens from different columns can not be placed
-		// in the same row, therefore the values
-		// must be different
-		for (int i = 0; i < queens.length; i++)
-			for (int j = i - 1; j >= 0; j--)
-				store.impose(new XneqY(queens[i], queens[j]));
-
-		// Notice that j index starts from i+1
-		for (int i = 0; i < queens.length; i++) {
-			for (int j = i + 1; j < queens.length; j++) {
-
-				// Temporary variable denotes the chessboard
-				// field in j-th column which is checked by
-				// i-th column queen
-				// If temporarty variable has value outside
-				// range 1..numberQ then i-th column queen
-				// does not check any field in j-th column
-
-				// Checking diagonals like this \
-				// Note that C constant is positive
-				IntVar temporary = new IntVar(store, -2 * numberQ, 2 * numberQ);
-				store.impose(new XplusCeqZ(queens[j], j - i, temporary));
-				store.impose(new XneqY(queens[i], temporary));
-
-				// Checking diagonals like this /
-				// Note that C constant is negative
-				temporary = new IntVar(store, -2 * numberQ, 2 * numberQ);
-				store.impose(new XplusCeqZ(queens[j], -(j - i), temporary));
-				store.impose(new XneqY(queens[i], temporary));
-
-			}
-		}
+		initializeProblem(store, vars, queens, numberQ);
+		
 
 		
-		TabuSearch tabusearch = new TabuSearch(store, queens);
+
+		System.out.println("\n\nCOMPLETE SEARCH :\n=============================");
+		long cs_begin = System.currentTimeMillis();
+		completeSearch(store, queens);
+		long cs_end = System.currentTimeMillis();
+		float cs_time = ((float) (cs_end-cs_begin)) / 1000f;
+		
+
+		store = new Store();
+		vars = new ArrayList<Var>();
+		queens = new IntVar[numberQ];
+
+		initializeProblem(store, vars, queens, numberQ);		
+		
+
+		System.out.println("\n\nTABU SEARCH :\n=============================");
+		TabuSearch tabusearch = new TabuSearch(queens);
+		long ts_begin = System.currentTimeMillis();
 		tabusearch.search();
+		long ts_end = System.currentTimeMillis();
+		float ts_time = ((float) (ts_end-ts_begin)) / 1000f;
+
+		
+		System.out.println("\n\nComparaison des temps d'exécution (s) :");
+		System.out.println("CompleteSearch : " + cs_time);
+		System.out.println("TabuSearch : " + ts_time);
+		
 
 	}
+	
+	
 
 }
--- src/TabuSearch.java
+++ src/TabuSearch.java
@@ -13,7 +13,6 @@ import JaCoP.search.*;
 
 public class TabuSearch {
 	
-	private Store store;
 	private IntVar[] vars;
 	private IntVar[] bestSolution;
 	private IntDomain[] domains;
@@ -22,9 +21,9 @@ public class TabuSearch {
 	int maxIteration = 900;
 	int maxIterationWhithoutBetterSolution = 100;
 	int tabuListMaxLength = 10;
+	int maxRestart = 10;
 
-	public TabuSearch(Store store, IntVar[] vars) {
-		this.store = store;
+	public TabuSearch(IntVar[] vars) {
 		this.vars = vars;
 		this.domains = new IntDomain[vars.length];
 		this.bestSolution = new IntVar[vars.length];
@@ -39,78 +38,92 @@ public class TabuSearch {
 			domains[i] = (IntDomain)vars[i].dom().clone();
 		}
 		
-		// Step 1 : generate a random solution
-		vars = this.generate_random();
-		
-		
-		// Step 2 : save the best known solution
-		bestSolution = this.cloneIntVar(vars);
-		
-		
-		// Step 3 : search solution while stopping conditions not met
-		int iterations = 0;
+		int nbRestart = 0;
 		boolean optimalSolutionFound = false;
-		int noBetterSolution = 0;
-		while(!optimalSolutionFound && (iterations < maxIteration) && (noBetterSolution < maxIterationWhithoutBetterSolution))
-		{		
-			// Updating values for stopping conditions
-			System.out.println("===============");
-			iterations++;
-			System.out.println("Iteration n°"+iterations);
+		while(nbRestart < maxRestart && !optimalSolutionFound) {
 			
-			
-			// Step 3.1 : search candidate solutions
-			ArrayList<IntVar[]> candidateSolutions = getCandidateSolutions(vars, domains, tabuList);
+			if(nbRestart > 0) 
+				System.out.println("Restarting...\n");
 		
-			// Step 3.2 : select best candidate solution
-			IntVar[] bestCandidateSolution = getBestSolution(candidateSolutions);
-			System.out.println("Best candidate solution :");
-			for(int i = 0 ; i < bestCandidateSolution.length ; i++) {
-				System.out.println(bestCandidateSolution[i]);
-			}
+			// Step 1 : generate a random solution
+			vars = this.generate_random();
 			
 			
-			// Step 3.3 update current solution
-			vars = cloneIntVar(bestCandidateSolution);
-		
-			// Step 3.4 : If current cost is better than cost of best known solution, update best known solution
-			if(getViolatedConstraints(bestCandidateSolution) < getViolatedConstraints(bestSolution)) {
-				bestSolution = bestCandidateSolution;
-				noBetterSolution = 0;
-			}
-			else {
-				noBetterSolution++;
-			}
-			
-			if(getViolatedConstraints(bestSolution) == 0) {
-				optimalSolutionFound = true;
-			}
-		
-			// Step 3.5 : update tabu list
-			tabuList.insert(bestCandidateSolution);
+			// Step 2 : save the best known solution
+			bestSolution = this.cloneIntVar(vars);
 			
 			
-			// Print the best solution
-			if(noBetterSolution == 0) {
-				System.out.println("New best solution found");
-				for(int i = 0 ; i < bestSolution.length ; i++) {
-					System.out.println(bestSolution[i]);
+			// Step 3 : search solution while stopping conditions not met
+			int iterations = 0;
+			int noBetterSolution = 0;
+			while(!optimalSolutionFound && (iterations < maxIteration) && (noBetterSolution < maxIterationWhithoutBetterSolution))
+			{		
+				// Updating values for stopping conditions
+				//System.out.println("===============");
+				iterations++;
+				//System.out.println("Iteration n°"+iterations);
+				
+				
+				// Step 3.1 : search candidate solutions
+				///TODO optimiser en prenant une permutation !! (si q1 va en 2, celui qui est déjà en 2 va a l'ancienne colonne de q1)
+				ArrayList<IntVar[]> candidateSolutions = getCandidateSolutions(vars, domains, tabuList);
+			
+				// Step 3.2 : select best candidate solution
+				IntVar[] bestCandidateSolution = getBestSolution(candidateSolutions);
+				/*System.out.println("Best candidate solution :");
+				for(int i = 0 ; i < bestCandidateSolution.length ; i++) {
+					System.out.println(bestCandidateSolution[i]);
+				}*/
+				
+				
+				// Step 3.3 update current solution
+				vars = cloneIntVar(bestCandidateSolution);
+			
+				// Step 3.4 : If current cost is better than cost of best known solution, update best known solution
+				if(getViolatedConstraints(bestCandidateSolution) < getViolatedConstraints(bestSolution)) {
+					bestSolution = bestCandidateSolution;
+					noBetterSolution = 0;
 				}
+				else {
+					noBetterSolution++;
+				}
+				
+				if(getViolatedConstraints(bestSolution) == 0) {
+					optimalSolutionFound = true;
+				}
+			
+				// Step 3.5 : update tabu list
+				/// TODO changer la tabu list pour ne mettre que des permutations
+				tabuList.insert(bestCandidateSolution);
+				
+				
+				/// TODO : aspirations condition (un mouvement tabou peut etre choisi si améliore le cout)
+				
+				
+				// Print the best solution
+				/*if(noBetterSolution == 0) {
+					System.out.println("New best solution found");
+					for(int i = 0 ; i < bestSolution.length ; i++) {
+						System.out.println(bestSolution[i]);
+					}
+				}*/
+				
 			}
 			
-		}
-		
-		String whyStopped = "";
-		if(optimalSolutionFound) 
-			whyStopped = " optimal solution found ";
-		else if(iterations >= maxIteration)
-			whyStopped = " max iterations reached ";
-		else if(noBetterSolution >= maxIterationWhithoutBetterSolution)
-			whyStopped = " no better solution found in " + noBetterSolution + "iterations ";
-		
-		System.out.println("\n\nEnd of search because" + whyStopped + ". Best solution is :");
-		for(int i = 0 ; i < bestSolution.length ; i++) {
-			System.out.println(bestSolution[i]);
+			String whyStopped = "";
+			if(optimalSolutionFound) 
+				whyStopped = " optimal solution found ";
+			else if(iterations >= maxIteration)
+				whyStopped = " max iterations reached ";
+			else if(noBetterSolution >= maxIterationWhithoutBetterSolution)
+				whyStopped = " no better solution found in " + noBetterSolution + "iterations ";
+			
+			System.out.println("\n\nEnd of search because" + whyStopped + ". Best solution is :");
+			for(int i = 0 ; i < bestSolution.length ; i++) {
+				System.out.println(bestSolution[i]);
+			}
+			
+			nbRestart++;
 		}
 		
 	}
@@ -120,9 +133,15 @@ public class TabuSearch {
 		
 		System.out.println("Generating random solution...");
 		
+		ArrayList<Integer> forbiddenValues = new ArrayList<Integer>(vars.length); // Alldiff on cols
+		
 		for(int i = 0 ; i < vars.length ; i++) {
 			int randomNum = domains[i].getRandomValue();
+			while(forbiddenValues.contains(new Integer(randomNum))) {
+				randomNum = domains[i].getRandomValue();
+			}
 			
+			forbiddenValues.add(new Integer(randomNum));
 			vars[i].setDomain(randomNum, randomNum);
 			System.out.println(vars[i]);
 		}
@@ -160,7 +179,7 @@ public class TabuSearch {
 		return candidateSolutions;
 	}
 	
-	public IntVar[] cloneIntVar(IntVar[] toClone) {
+	public static IntVar[] cloneIntVar(IntVar[] toClone) {
 		IntVar[] cloned = new IntVar[toClone.length];
 
 		for(int i = 0 ; i < toClone.length ; i++) {
